bfa9b132d9dcb2361640d304c2e493fbbb781638
Jack Cherng <jfcherng@gmail.com>
2020-10-30 11:17:28 +0800

    style: format with black
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -25,11 +25,9 @@ def lsp_expand_variables(window, var):
 
 
 class LspIntelephensePlugin(NpmClientHandler):
-    package_name = __package__
-    server_directory = 'language-server'
-    server_binary_path = os.path.join(
-        server_directory, 'node_modules', 'intelephense', 'lib', 'intelephense.js'
-    )
+    package_name = __package__.partition(".")[0]
+    server_directory = "language-server"
+    server_binary_path = os.path.join(server_directory, "node_modules", "intelephense", "lib", "intelephense.js")
 
     @classmethod
     def on_client_configuration_ready(cls, configuration: dict) -> None:
@@ -38,8 +36,8 @@ class LspIntelephensePlugin(NpmClientHandler):
             configuration.get("initializationOptions", {}))
 
     def on_ready(self, api) -> None:
-        api.on_notification('indexingStarted', lambda params: self._handle_indexing_status('started'))
-        api.on_notification('indexingEnded', lambda params: self._handle_indexing_status('finished'))
+        api.on_notification("indexingStarted", lambda params: self._handle_indexing_status("started"))
+        api.on_notification("indexingEnded", lambda params: self._handle_indexing_status("finished"))
 
     def _handle_indexing_status(self, status) -> None:
-        sublime.status_message('Intelephense: Indexing {}'.format(status))
+        sublime.status_message("Intelephense: Indexing {}".format(status))
